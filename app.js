1054425501a3fb6884b7ec229ff5acf9fad74111
BmmNdr <abiemmibi@gmail.com>
2022-09-07 18:16:51 +0200

    added change of color on click

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const GRID = document.getElementById('grid');
 
 const GRID_DIM = 16; //number of squares in the grid
-const SQUARE_DIM = 25; //dimension of squares' side
+const SQUARE_DIM = 400/GRID_DIM; //dimension of squares' side
 
 GRID.setAttribute('style', 'grid-template-columns: repeat('+GRID_DIM+', auto)');
 
 for (i = 0; i < GRID_DIM * GRID_DIM; i++) {
     //create a div square to put inside the grid
     const square = document.createElement('div');
-    square.setAttribute('style', 'height: '+SQUARE_DIM+'px; width: '+SQUARE_DIM+'px; border: solid 1px black');
+    square.setAttribute('style', 'height: '+SQUARE_DIM+'px; width: '+SQUARE_DIM+'px; border: solid 1px gray');
+
+    square.addEventListener('click', changeColor);
 
     //put the square in the grid
     GRID.appendChild(square);
+}
+
+function changeColor(e) {
+    e.target.style.backgroundColor = 'black';
+    e.target.style.border = 'solid 1px black';
 }
