f39e24e5e91b10a1473c43b009d01db5ab76d9ed
BmmNdr <abiemmibi@gmail.com>
2022-09-07 22:38:55 +0200

    added grid dimension selector and modifier

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 const GRID = document.getElementById('grid');
+const SLIDER = document.getElementById('slider');
 
-const GRID_DIM = 16; //number of squares in the grid
-const SQUARE_DIM = 400/GRID_DIM; //dimension of squares' side
+var GRID_DIM = SLIDER.value; //number of squares in the grid
+var SQUARE_DIM = 400/GRID_DIM; //dimension of squares' side
 
-GRID.setAttribute('style', 'grid-template-columns: repeat('+GRID_DIM+', auto)');
+SLIDER.addEventListener('input', newGrid);
 
-for (i = 0; i < GRID_DIM * GRID_DIM; i++) {
-    //create a div square to put inside the grid
-    const square = document.createElement('div');
-    square.setAttribute('style', 'height: '+SQUARE_DIM+'px; width: '+SQUARE_DIM+'px; border: solid 1px gray');
+createGrid();
 
-    square.addEventListener('click', changeColor);
+function createGrid()
+{
+    GRID.setAttribute('style', 'grid-template-columns: repeat('+GRID_DIM+', 1fr); grid-template-rows: repeat('+GRID_DIM+', 1fr)');
 
-    //put the square in the grid
-    GRID.appendChild(square);
+    for (i = 0; i < GRID_DIM * GRID_DIM; i++) {
+        //create a div square to put inside the grid
+        const square = document.createElement('div');
+        square.setAttribute('style', 'height: '+SQUARE_DIM+'px; width: '+SQUARE_DIM+'px');
+    
+        square.addEventListener('click', () => {
+            square.style.backgroundColor = 'black';
+        });
+    
+        //put the square in the grid
+        GRID.appendChild(square);
+    }
 }
 
-function changeColor(e) {
-    e.target.style.backgroundColor = 'black';
-    e.target.style.border = 'solid 1px black';
+function newGrid() {
+    GRID_DIM = SLIDER.value;
+    SQUARE_DIM = 400/GRID_DIM;
+
+    removeGrid();
+    createGrid();
+}
+
+function removeGrid() {
+    while(GRID.firstChild) {
+        GRID.removeChild(GRID.lastChild);
+    }
 }
