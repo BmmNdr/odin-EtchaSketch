6f6d616818ca118cd6e2cc42f99ddcb09019547f
BmmNdr <abiemmibi@gmail.com>
2022-09-08 13:36:50 +0200

    added clear button

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const GRID = document.getElementById('grid');
 const SLIDER = document.getElementById('slider');
 const SLIDER_DIM = document.getElementById('slider-dim');
 const COLOR_PICKER = document.getElementById('color-picker')
+const CLEAR_BUTTON = document.getElementById('clear-button');
+const SQUARES = document.getElementsByClassName('squares')
 
 const GRID_MAX = 600; //grid's dimension in px
 
@@ -15,20 +17,24 @@ document.body.onmouseup = () => (mouseDown = false);
 
 SLIDER.addEventListener('input', newGrid);
 COLOR_PICKER.addEventListener('input', () => {color = COLOR_PICKER.value});
+CLEAR_BUTTON.addEventListener('click', clearGrid);
 
 createGrid();
 function createGrid()
 {
     GRID.setAttribute('style', 'grid-template-columns: repeat('+gridDim+', 1fr); grid-template-rows: repeat('+gridDim+', 1fr)');
 
-    for (i = 0; i < gridDim * gridDim; i++) {
+    for (var i = 0; i < gridDim * gridDim; i++) {
         //create a div square to put inside the grid
         const square = document.createElement('div');
+        
         //square.setAttribute('style', 'height: '+squareDim+'px; width: '+squareDim+'px');
         square.setAttribute('style', 'height: '+squareDim+'px; width: '+squareDim+'px');
     
         square.addEventListener('click', changeColor);
         square.addEventListener('mouseover', changeColor);
+
+        square.classList.toggle('squares');
     
         //put the square in the grid
         GRID.appendChild(square);
@@ -46,9 +52,8 @@ function newGrid() {
 }
 
 function removeGrid() {
-    while(GRID.firstChild) {
+    while(GRID.firstChild)
         GRID.removeChild(GRID.lastChild);
-    }
 }
 
 function changeColor(e) {
@@ -56,4 +61,10 @@ function changeColor(e) {
         return
 
     e.target.style.backgroundColor = color;
+}
+
+function clearGrid() {
+
+    for(var i = 0; i < SQUARES.length; i++)
+        SQUARES[i].style.backgroundColor = 'white';
 }
