a5c5c7435efac719d21b72ba4db97012108f01c0
BmmNdr <abiemmibi@gmail.com>
2022-09-08 12:10:49 +0200

    change color dragging mouse

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const GRID = document.getElementById('grid');
 const SLIDER = document.getElementById('slider');
 
-var GRID_DIM = SLIDER.value; //number of squares in the grid
-var SQUARE_DIM = 400/GRID_DIM; //dimension of squares' side
+const GRID_MAX = 600; //grid's dimension in px
+
+var gridDim = SLIDER.value; //number of squares in the grid
+var squareDim = GRID_MAX/gridDim; //dimension of squares' side
+var mouseDown = false;
+
+document.body.onmousedown  = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 
 SLIDER.addEventListener('input', newGrid);
 
@@ -10,16 +17,16 @@ createGrid();
 
 function createGrid()
 {
-    GRID.setAttribute('style', 'grid-template-columns: repeat('+GRID_DIM+', 1fr); grid-template-rows: repeat('+GRID_DIM+', 1fr)');
+    GRID.setAttribute('style', 'grid-template-columns: repeat('+gridDim+', 1fr); grid-template-rows: repeat('+gridDim+', 1fr)');
 
-    for (i = 0; i < GRID_DIM * GRID_DIM; i++) {
+    for (i = 0; i < gridDim * gridDim; i++) {
         //create a div square to put inside the grid
         const square = document.createElement('div');
-        square.setAttribute('style', 'height: '+SQUARE_DIM+'px; width: '+SQUARE_DIM+'px');
+        //square.setAttribute('style', 'height: '+squareDim+'px; width: '+squareDim+'px');
+        square.setAttribute('style', 'height: '+squareDim+'px; width: '+squareDim+'px');
     
-        square.addEventListener('click', () => {
-            square.style.backgroundColor = 'black';
-        });
+        square.addEventListener('click', changeColor);
+        square.addEventListener('mouseover', changeColor);
     
         //put the square in the grid
         GRID.appendChild(square);
@@ -27,8 +34,8 @@ function createGrid()
 }
 
 function newGrid() {
-    GRID_DIM = SLIDER.value;
-    SQUARE_DIM = 400/GRID_DIM;
+    gridDim = SLIDER.value;
+    squareDim = GRID_MAX/gridDim;
 
     removeGrid();
     createGrid();
@@ -38,4 +45,9 @@ function removeGrid() {
     while(GRID.firstChild) {
         GRID.removeChild(GRID.lastChild);
     }
+}
+
+function changeColor(e) {
+    if(mouseDown)
+        e.target.style.backgroundColor = 'black';
 }
